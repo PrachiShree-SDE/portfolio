b8b1a9858283ba123a89ea07b7897da51daf8a52
PrachiShree-SDE <prachishree111@gmail.com>
2026-06-25 12:19:45 +0530

    resume updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -107,7 +107,7 @@ const About = () => {
             <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
 
               <a
-                href="https://drive.google.com/file/d/1f1idLARHhZ_66gaVYIZzcKjuFMLHkEfP/view?usp=sharing"
+                href="https://drive.google.com/file/d/1VXhjBx4CKJ4sXsJUjo1wYzKFjQOJqFMi/view?usp=sharing"
                 target="_blank"
                 rel="noreferrer"
                 className="inline-flex items-center justify-center gap-2 px-8 py-4 bg-blue-600 text-white rounded-2xl font-bold shadow-lg hover:bg-blue-700 hover:-translate-y-1 transition-all active:scale-95"
